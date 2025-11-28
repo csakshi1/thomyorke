c570a1912a0b24769bee3c74fd9cfa92f9584541
Sakshi Choudhary <csakshi1@gmail.com>
2025-11-28 23:40:00 +0530

    cleanup

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
-import simpleGit from "simple"
+import { simpleGit } from "simple-git";
 
 const path = "./data.json";
-const date = moment().subtract(1,'d').format();
+const git = simpleGit();
 
-const data = {
-    date: date,
-};
+async function createCommit(day) {
+    const date = moment().subtract(day, "d").format();
+    const data = { date };
 
-jsonfile.writeFile(path,data, ()=>{
-    simpleGit().add([path]).commit(date,{'--date' :date}).push();
-});
+    jsonfile.writeFile(path, data, async () => {
+        await git.add(path);
+        await git.commit(date, { '--date': date });
+        await git.push();
+        console.log(`Commit done for ${date}`);
+    });
+}
 
-simpleGit().add([path]).commit(date,{'--date' :date}).push();
+for (let day = 30; day >= 1; day--) {   // change 30 to any number of required past days
+    createCommit(day);
+}
